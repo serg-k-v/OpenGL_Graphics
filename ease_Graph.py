c90b67ad2dccb1334251ecdfe3ad25dcbb77794b
akuma <sergeyvladimirovish@gmail.com>
2019-02-16 01:44:40 +0300

    works point

--- ease_Graph.py
+++ ease_Graph.py
@@ -2,6 +2,8 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
+width, height = 640,480
+
 def draw_point( size, color, point_coord, point_smooth ) :
     glPointSize(size)
     glColor3d(color[0], color[1], color[2])
@@ -11,26 +13,31 @@ def draw_point( size, color, point_coord, point_smooth ) :
         glDisable(GL_POINT_SMOOTH)
     glBegin(GL_POINTS)
     glVertex3d(point_coord[0], point_coord[1], point_coord[2])
+    glVertex3d(point_coord[0], point_coord[1], point_coord[2])
     glEnd()
-    return
+
+
 
 def draw() :
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
-    refresh2d(640, 480)
-    draw_point(400, [1.0, 1.0, 0], [10, 20, 0], False)
+
+    draw_point(10, [.1, .1, .5], [100, 200, 0], False)
+    draw_point(20, [.1, .17, .5], [50, 100, 0], False)
+    draw_point(40, [.3, .5, 0.5], [150, 400, 0], False)
+
     glutSwapBuffers()
 
 def refresh2d(width, height):
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(0.0, width, 0.0, height, 0.0, 1.0)
+    glOrtho(0.0, width, 0.0, height, -1.0, 1.0)
     glMatrixMode (GL_MODELVIEW)
-
+    glutPostRedisplay()
 
 def Init_GL_Window(num_window, width, height) :
-    global window # must be around glutCreateWindow
+
     glutInit()
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
     glutInitWindowSize(width, height)
@@ -39,13 +46,15 @@ def Init_GL_Window(num_window, width, height) :
     window = glutCreateWindow("My First app on OpenGL")
 
     glutDisplayFunc(draw)
-    glutIdleFunc(draw)
+    # glutIdleFunc(draw)
+    glutReshapeFunc(refresh2d)
+    glClearColor(.26, .23, .51 , 1.0) # background color
     glutMainLoop()
 
 
 
 def main() :
-    Init_GL_Window(0, 640,480);
+    Init_GL_Window(0, width, height);
     print('I do it')
     # draw_point(5, [1, 0, 0], [4.1, 6, 0], True)
 
