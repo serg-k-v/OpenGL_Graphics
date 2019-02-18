788f145d9c8a4b8e2d0e9f021c70c7f9f7f15a6b
akuma <sergeyvladimirovish@gmail.com>
2019-02-18 22:12:46 +0300

    add triangle

--- ease_Graph.py
+++ ease_Graph.py
@@ -24,25 +24,49 @@ def draw_line(width_line, color, beg_coord, end_coord, point_smooth) :
     glVertex3d(end_coord[0], end_coord[1], end_coord[2])
     glEnd()
 
-
-def draw_line_strip(width_line, color, beg_coord, *end_coord, point_smooth) :
+def draw_line_strip(width_line, color, beg_coord, end_coord, point_smooth, *addit_end_coord) :
     glLineWidth(width_line)
     glBegin(GL_LINE_STRIP)
     # base line
     glColor3d(color[0], color[1], color[2])
     glVertex3d(beg_coord[0], beg_coord[1], beg_coord[2])
-    glVertex3d(-1,3,0)
+    glVertex3d(end_coord[0], end_coord[1], end_coord[2])
     # addition line
-    for  in variable:
-        pass
-    glColor3d(0,1,0)
-    glVertex3d(-1.5,3.3,0)
-    glColor3d(0,0,1)
-    glVertex3d(-1,3.5,0)
+    for coord  in addit_end_coord:
+        glColor3d(0, 1, 0)
+        glVertex3d(coord[0], coord[1], coord[2])
     glEnd()
 
-def draw_triangles() :
-    print('don\'t work')
+def draw_line_loop(width_line, color, beg_coord, end_coord, point_smooth, *addit_end_coord) :
+    glLineWidth(width_line)
+    glBegin(GL_LINE_LOOP)
+    # base line
+    glColor3d(color[0], color[1], color[2])
+    glVertex3d(beg_coord[0], beg_coord[1], beg_coord[2])
+    glVertex3d(end_coord[0], end_coord[1], end_coord[2])
+    # addition line
+    for coord  in addit_end_coord:
+        glColor3d(0, 1, 0)
+        glVertex3d(coord[0], coord[1], coord[2])
+    glEnd()
+    pass
+
+def draw_triangles(width_line, color, coord, point_smooth, tr_type) :
+    glLineWidth(width_line)
+    print(GL_TRIANGLES)
+    if tr_type == 'fill' :
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+    elif tr_type == 'line' :
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    elif tr_type == 'point' :
+        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT)
+    else :
+        print("Error arguments!")
+    glBegin(GL_TRIANGLES)
+    glColor3d(color[0], color[1], color[2])
+    for el in coord:
+        glVertex3d(el[0], el[1], el[2])
+    glEnd()
 
 def draw() :
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -51,19 +75,17 @@ def draw() :
     draw_point(10, [.1, .1, .5], [100, 200, 0], False)
     draw_point(20, [.1, .17, .5], [50, 100, 0], False)
     draw_point(40, [.3, .5, 0.5], [150, 400, 0], True)
-    draw_line(5, [1., 1., 1.], [50, 50, 0], [300, 100, 0], False)
-    draw_line(5, [1., 1., 1.], [300, 100, 0], [500, 400, 0], False)
 
-    glLineWidth(20)
-    glBegin(GL_LINE_STRIP)
-    glColor3d(1,0,0)
-    glVertex3d(50, 200, 0)
-    glVertex3d(200, 350, 0)
-    glColor3d(0,1,0)
-    glVertex3d(250, 550,0)
-    glColor3d(0,0,1)
-    glVertex3d(200, 380,0)
-    glEnd()
+    draw_line(5, [1., .5, 1.], [50, 50, 0], [300, 100, 0], False)
+    draw_line(5, [1., 1., 1.], [300, 50, 0], [500, 400, 0], False)
+
+    draw_line_strip(20, [1, 0, 0], [50, 200, 0], [200, 350, 0], False,
+    [250, 550,0], [400, 380,0], [150, 60, 0])
+
+    draw_line_loop(20, [0, 1, 0.9], [0, 220, 0], [180, 380, 0], False,
+    [290, 590,0], [420, 390,0], [190, 130, 0])
+
+    draw_triangles(15, [0.2, 0.3, 0.8], [[500, 500, 0],[20, 20, 0],[600, 20, 0]], False, 'fill')
 
     glutSwapBuffers()
 
