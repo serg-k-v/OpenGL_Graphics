5999181b307403bb681c3c268309a29c6af4de72
akuma <sergeyvladimirovish@gmail.com>
2019-03-11 02:01:39 +0300

    lab 3

--- lab_3.py
+++ lab_3.py
@@ -5,29 +5,57 @@ from OpenGL.GLUT import *
 from gl_implement.gl_primitives import draw_triangle
 
 import numpy as np
-width, height = 900,700
+width, height = 900,900
 
-tr_side = .6
-alpha = -np.pi/2
-tr_coords = np.array([[0,0,0], [tr_side,0,0], [tr_side/2, (6/5)*np.sqrt(3/4)*tr_side, 0]])
+tr_side = 1.5
+alpha = np.pi/2
+
 rotate_mtr_on_z = np.array([[np.cos(alpha),-np.sin(alpha),0],[np.sin(alpha),np.cos(alpha),0],[0,0,1]])
-# shift = np.array([0,tr_side,0])
+
+shift_right = lambda ord : np.array([(11/18)*tr_side*(3/5), 0, 0])
+shift_up = lambda ord : np.array([0, (0.34)*tr_side*(3/5), 0])
+shift_down = lambda ord : np.array([0, -(0.34)*tr_side*(3/5), 0])
 
 def rotation(coords) :
     return coords.dot(rotate_mtr_on_z)
+
+def draw_fractal(order, max_order, tr_coords) :
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    for coord in tr_coords:
+        draw_triangle(3., [.1, .5, .1, 1], coord, False, 'line')
+
+    tr_coords = rotation(tr_coords)*0.6
+    new_obj = tr_coords.copy()
+    tr_coords = np.concatenate((tr_coords + shift_up(order), tr_coords + shift_down(order)))
+    tr_coords = np.concatenate((tr_coords, new_obj + shift_right(order)))
+
+    order += 1
+    if order != max_order :
+        return draw_fractal(order, max_order, tr_coords)
+
 
 def draw() :
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
 
-    for el in range(length(coords)):
-        draw_triangle(3., [.1, .5, .1, 1], tr_coords, False, 'line')
-        
-    shift_left = np.array([(2/3)*tr_side, 0, 0])
-    draw_triangle(3., [.1, .5, .1, 1], tr_coords + shift_left, False, 'line')
-    # shift_up = np.array([0, (2/3)*((np.sqrt(3)/2)*tr_side), 0])
-    shift_up = np.array([0, (2/3.5  )*tr_side, 0])
-    draw_triangle(3., [.1, .5, .1, 1], tr_coords + shift_up +shift_left/2, False, 'line')
+    max_order = 9
+    tr_coords = np.array([[
+    [-tr_side/2, -tr_side/2, 0],
+    [tr_side/2, -tr_side/2, 0],
+    [0, tr_side/2, 0]
+    ]])
+
+
+    draw_fractal(0,max_order, tr_coords)
+
+    glPointSize(3)
+    glColor3d(255,255,255)
+    glBegin(GL_POINTS)
+    glVertex3f(0,0,0)
+    glEnd()
+    # draw_triangle(3., [.1, .5, .1, 1], tr_coords + shift_left, False, 'line')
+    # draw_triangle(3., [.1, .5, .1, 1], tr_coords + shift_up +shift_left/2, False, 'line')
     glutSwapBuffers()
 
 def refresh2d(width, height):
