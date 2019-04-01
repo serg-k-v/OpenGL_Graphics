622aaf2597bb2433753779df787efd4dafb54ee6
akuma <sergeyvladimirovish@gmail.com>
2019-04-01 17:24:35 +0300

    lab 5 done

--- lab_5.py
+++ lab_5.py
@@ -12,9 +12,12 @@ global pointcolor
 
 width, height = 800,600
 
+
+
 def gen_points() :
     x = np.array(np.arange(-0.8, 0.8, 0.01))
-    y = 0.13*np.sin(10*x)
+    y = 0.13*np.sin(10*(x))
+    # y = np.zeros((len(x)))
     z = np.array([0.8]*len(x))
     coords = np.empty((len(x),3))
     for i in range(len(x)):
@@ -24,9 +27,7 @@ def gen_points() :
             coords[i] = [x[i], y[i], -z[i]]
     return coords
 
-# pointdata = [[-0.5, -0.5, 0], [0.5, -0.5, 0], [0.5, 0.5, 0], [-0.5, 0.5, 0]]
 pointdata = gen_points()
-# print(pointdata)
 pointcolor = [[1, 1, 0], [0, 1, 1], [1, 0, 1]]
 
 def compile_shader(type, sourse) :
@@ -61,28 +62,64 @@ def create__shader(vertexShader, fragmentShader) :
 def draw():
     glClear(GL_COLOR_BUFFER_BIT)                    # Очищаем экран и заливаем серым цветом
     glEnableClientState(GL_VERTEX_ARRAY)            # Включаем использование массива вершин
+    glEnableClientState(GL_COLOR_ARRAY)
+    glColorPointer(3, GL_FLOAT, 0, pointcolor)
+
     glVertexPointer(3, GL_FLOAT, 0, pointdata)
     glDrawArrays(GL_QUAD_STRIP, 0, len(pointdata))
+
     glDisableClientState(GL_VERTEX_ARRAY)           # Отключаем использование массива вершин
     glDisableClientState(GL_COLOR_ARRAY)            # Отключаем использование массива цветов
 
     vertexShader = """
+    uniform float Time;
+    uniform float  K;
+    uniform float Velocity;
+    uniform float Amp;
+    //uniform mat3 NormalMatrix;
+    //uniform mat4 MVP;
+
     void main(){
-        gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+        //out vec4 Position;
+        //out vec3 Normal;
+
+        vec4 pos = vec4(vec3(gl_Vertex), 1.0);
+        pos.y = Amp * sin( K * (pos.x - Velocity * Time) );
+
+        //vec3 n = vec3(0.0);
+        //n.xy = normalize(vec2(-K * Amp * cos(u), 1.0));
+
+        gl_Position = gl_ModelViewProjectionMatrix * pos;
+        //Normal = NormalMatrix * n;
     }
     """
     fragmentShader = """
-    uniform float u_time;
+    uniform float Time;
     void main() {
-    	gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+    	gl_FragColor = vec4( abs(sin(Time)), 0.0, 0.0, 1.0);
     }"""
 
     shader = create__shader(vertexShader, fragmentShader)
     glUseProgram(shader)
 
     time = (glutGet(GLUT_ELAPSED_TIME)/1000.)
-    location = glGetUniformLocation(shader, 'u_time')
-    glUniform1f(location, time)
+    K = 10.0
+    Velocity = 1.0
+    Amp = 0.13
+
+    # location = glGetUniformLocation(shader, 'Time')
+    # glUniform1f(location, time)
+    uniforms = {
+        'time': glGetUniformLocation(shader, 'Time'),
+        'K': glGetUniformLocation(shader, 'K'),
+        'Velocity': glGetUniformLocation(shader, 'Velocity'),
+        'Amp': glGetUniformLocation(shader, 'Amp'),
+        }
+    glUniform1f(uniforms['time'], time)
+    glUniform1f(uniforms['K'], K)
+    glUniform1f(uniforms['Velocity'], Velocity)
+    glUniform1f(uniforms['Amp'], Amp)
+
 
     glutSwapBuffers()                               # Выводим все нарисованное в памяти на экран
 
