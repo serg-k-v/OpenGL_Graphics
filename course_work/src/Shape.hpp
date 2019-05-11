2a76143f4b2ad3fccbaebd4fbdf098231078b1eb
akuma <sergeyvladimirovish@gmail.com>
2019-05-11 04:05:21 +0300

    draw circle

--- course_work/src/Shape.hpp
+++ course_work/src/Shape.hpp
@@ -1,8 +1,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <vector>
-
-
+#include <iostream>
+#include <cmath>
+// #define M_PI 3.141592653
 class Shape {
 private:
 
@@ -31,24 +32,29 @@ public:
     std::vector<int> create_indices(){
         std::vector<int> indices(3*sectors);
         for (size_t i = 0; i < sectors; i++) {
-            indices[i+3] = 0;
-            indices[i+1] = i+1;
-            indices[i+2] = i+2;
+            indices[3*i] = 0;
+            indices[3*i+1] = i+1;
+            indices[3*i+2] = i+2 <= sectors ? i+2 : 1 ;
         }
         return indices;
     }
-
     std::vector<float> create_point(){
-        std::vector<float> points(3*(sectors + 1));
+        std::vector<float> points;
         for (size_t i = 0; i < 3; i++) {
-            points[i] = center[i];
+            points.push_back(center[i]);
         }
-        for (size_t i = 3; i < points.size(); i+=3) {
-            points[i] = center[0] + radius * glm::cos( i * 2 * glm::pi<float>() / sectors );
-            points[i+1] = center[1] + 0;
-            points[i+2] = center[2] + radius * glm::sin( i * 2 * glm::pi<float>()/ sectors );
+        std::cout << "/* message */" << '\n';
+        for (size_t i = 0; i < sectors; i++) {
+            std::cout << i << ' '
+                      << cos( float(i * 2 * M_PI / sectors) ) << ' '
+                      << sin( float(i * 2 * M_PI / sectors) ) << '\n';
+            points.push_back(center[0] + radius * cos( i * 2 * M_PI / sectors ));
+            points.push_back(center[1] + radius * sin( i * 2 * M_PI/ sectors ));
+            points.push_back(center[2] + 0);
         }
 
         return points;
     }
+
+    
 };
--- course_work/src/main.cpp
+++ course_work/src/main.cpp
@@ -9,8 +9,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 900;
+const unsigned int SCR_HEIGHT = 900;
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -97,26 +97,11 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    Circle* circle = new Circle(glm::vec3(0,0,0), 0.5, 4);
+    Circle* circle = new Circle(glm::vec3(0,0,0), 0.5, 100);
     std::vector<float> vertices_circle = circle->create_point();
     std::vector<int> indices_circle = circle->create_indices();
 
-    for (auto &el : vertices_circle) {
-        std::cout << el << ' ';
-    }
-    for (auto &el :  indices_circle) {
-        std::cout << el << ' ';
-    }
-    float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
+
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -125,10 +110,10 @@ int main()
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices_circle.size()*sizeof(vertices_circle[0]), vertices_circle.data(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_circle.size()*sizeof(indices_circle[0]), indices_circle.data(), GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -164,7 +149,7 @@ int main()
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, vertices_circle.size(), GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
